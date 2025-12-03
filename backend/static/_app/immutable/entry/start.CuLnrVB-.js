4fec41ef1a2476e7ea7812d001052bbaf9560c39
Hugh McIntyre <hugh@hughtec.com>
2025-12-03 14:16:57 +1100

    build: update frontend static assets for v2.3.2

--- backend/static/_app/immutable/entry/start.CuLnrVB-.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/B1Yp9NyN.js";export{o as load_css,r as start};
---
+++ backend/static/_app/immutable/entry/start.pKdM3X70.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/C34tcSnU.js";export{o as load_css,r as start};
